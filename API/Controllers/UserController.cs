8df3998f52e623e440e3cbaffb2d9b8d4570ad8b
Ayham Farhat <ayhamfarhat.pro@gmail.com>
2024-02-10 11:36:09 +0200

    End OF Section 2

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/user  ---->  // i can replace it to any custom path
+public class UserController : ControllerBase
+{
+    private readonly DataContext _context;
+    public UserController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")] // api/user/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+}
+
+
+
+
+// [HttpGet]
+//     public ActionResult<IEnumerable<AppUser>> GetUsers()
+//     {
+//         return _context.Users.ToList();
+//     }
+
+//     [HttpGet("{id}")] // api/user/2
+//     public ActionResult<AppUser> GetUser(int id)
+//     {
+//         return _context.Users.Find(id);
+//     }
